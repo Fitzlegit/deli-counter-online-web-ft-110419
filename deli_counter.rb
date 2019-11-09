09fed71d974df3cf6cee2cba4437fd3268a57e20
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-09 13:12:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
+require 'pp'
+
 # Write your code here.
+def line(katz_deli)
+ i = 0
+ num = 1
+  while i < katz_deli.length
+    puts "The line currently #{num}. #{katz_deli}"
+    
+    i += 1
+    num += 1
+  end
+  puts "The line is currently empty."
+end
+
+
+def take_a_number(katz_deli, name)
+  
+  
+end
+
+def now_serving(katz_deli)
+  
+  
+end
